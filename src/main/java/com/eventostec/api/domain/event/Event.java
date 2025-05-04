330ce95079c17e78d7e9e44dfddd1edfe1f86c19
felipesvm <federt49@gmail.com>
2025-05-04 11:10:26 -0300

    Primeito commit + class Event

---
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.event;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private boolean remote;
+    private Date date;
+
+}
